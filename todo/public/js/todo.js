c7cd8790f181e62c506def3d8a8e317d64f1d0b2
ika.grabon <ika.grabon@gmail.com>
2017-12-28 21:19:54 +0100

    Add select item id

--- todo/public/js/todo.js
+++ todo/public/js/todo.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     console.log("ika");
-    $(".done").click(function(e) {
+    $(".done").click(function(evnt) {
         console.log("Done");
+        var item_id = $(this).parents('li').attr('id');
+        console.log(item_id);
     })
 });
