fb9d6039848d6b0d117bd4bc129f35691a6aeb05
ika.grabon <ika.grabon@gmail.com>
2017-12-28 13:49:30 +0100

    Add class Item

---
+++ todo/app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'data_mapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
+
+class Item
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :done, Boolean, :required => true, :default => false
+  property :created, DateTime
+
+end
+
+DataMapper.finalize.auto_upgrade!
