a35cb6ca18e29dd0ceeaf4f4467150c104d4f2b3
ika.grabon <ika.grabon@gmail.com>
2017-12-28 21:26:18 +0100

    Add change done to not ready

--- todo/public/js/todo.js
+++ todo/public/js/todo.js
@@ -4,5 +4,22 @@ $(document).ready(function() {
         console.log("Done");
         var item_id = $(this).parents('li').attr('id');
         console.log(item_id);
-    })
+        $.ajax({
+            type: "POST",
+            url: "/done",
+            data: { id: item_id },
+        }).done(function(data) {
+            if(data.status == 'done') {
+                $("#" + data.id + " a.done").text('Not ready yet')
+                $("#" + data.id + " .item").wrapInner("<del>");
+            }
+            else {
+                $("#" + data.id + " a.done").text('Done')
+                $("#" + data.id + " .item").html(function(i, h) {
+                    return h.replace("<del>", "");
+                });
+            }
+        });
+        evnt.preventDefault();
+    });
 });
