38c4129b4402187111f8bf9598e9f31be5e87b59
ika.grabon <ika.grabon@gmail.com>
2017-12-28 18:11:29 +0100

    Change directory

--- todo/app.rb
+++ todo/app.rb
@@ -16,11 +16,16 @@ DataMapper.finalize.auto_upgrade!
 get '/' do
   @items = Item.all(:order => :created.desc)
   @title = "Should I stay or should I go?"
-  redirect '/new' if @items.empty?
+  # redirect '/new' if @items.empty?
   erb :index
 end
 
 get '/new' do
   @title = "Eggos? Add new task"
   erb :new
+end
+
+post '/new' do
+  Item.create(:content => params[:content], :created => Time.now)
+  redirect '/'
 end
